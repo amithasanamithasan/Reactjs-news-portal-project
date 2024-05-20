7cb093ada9c1737ab66ef8d89b150de2a7b0897b
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-05-21 00:22:18 +0600

    ovsebing user sing_in or sing_out onAuthStateChanged

--- src/Provider/AuthProviders.jsx
+++ src/Provider/AuthProviders.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState} from "react";
 // import { getAuth } from "firebase/auth";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 
@@ -9,17 +9,31 @@ export const AuthContext = createContext(null);
 
 const auth =getAuth(app);
 
-// create register with firebase
-const createUser= (email,password)=>{
-    return createUserWithEmailAndPassword(auth,email,password)
-}
 
-// observebing user sing in or singout session stage
+
+
 
 const AuthProviders = ({children}) => {
+
     const [user,setUser]=useState(null);
 
+// create register with firebase
+    const createUser= (email,password)=>{
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
 
+    
+// ovsebing user sing_in or sing_out onAuthStateChanged
+    useEffect(()=>{
+    
+   const unsubscribe=  onAuthStateChanged(auth,currentUser=>{
+        console.log('user alreadey exist or not ',currentUser);
+        setUser(currentUser);
+     });
+     return()=>{
+        unsubscribe();
+     }
+    },[])
 
 
     const authInfo={
