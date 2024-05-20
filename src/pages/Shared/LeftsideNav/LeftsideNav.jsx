c968e14b0787351ecd8fa0e97423858e3dd9c037
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-05-20 20:40:49 +0600

    Left Side Navigation with categories json file fetch id and name with image and icons

--- src/pages/Shared/LeftsideNav/LeftsideNav.jsx
+++ src/pages/Shared/LeftsideNav/LeftsideNav.jsx
@@ -1,10 +1,70 @@
-
+import { MdOutlineSportsSoccer } from "react-icons/md";
+import { CiCalendarDate } from "react-icons/ci";
+import moment from 'moment';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import  category1  from "../../../assets/1.png";
+import  category2  from "../../../assets/2.png";
+import  category3  from "../../../assets/3.png";
 
 const LeftsideNav = () => {
+    const [categories,setCategories]=useState([]);
+
+    useEffect(()=>{
+  fetch('categories.json')
+  .then(res=> res.json())
+  .then(data=> setCategories(data))
+
+
+    }, [])
+
+
+
     return (
-        <div>
-            <h1 className="text-2xl">Leftside nav</h1>
-        </div>
+       <div>
+ <h2 className="uppercase text-2xl font-serif text-lime-500 text-center ">All Caterogy</h2>
+ <div className="border-collapse border-spacing-2 rounded-xl space-y-4">
+   <h3 className="uppercase text-center py-11 font-serif  text-slate-400 text-3xl" >National_News</h3>
+    {
+        categories.map(categori=><Link className=" block ml-4 text-xl gap-10 text-blue-700"
+         key={categori.id} 
+         to={(`/categori/${categori.id}`)}>
+         {categori.name} 
+        </Link>)
+    }
+    <div>
+        
+     <h1>  <img src={category1} alt="" /> 
+     Bayern Slams Authorities Over Flight Delay to Club World Cup</h1>
+     <p className="flex gap-2 px-2 p-3 m-2">
+    <MdOutlineSportsSoccer className="text-2xl">
+    </MdOutlineSportsSoccer>sports</p> 
+ <p className="flex "><CiCalendarDate className="m-1 text-3xl"></CiCalendarDate> 
+          {moment().format("dddd, MMMM D YYYY ")} </p> 
+
+
+
+     <h1>  <img src={category2} alt="" />
+      Bayern Slams Authorities Over Flight Delay to Club World Cup</h1>
+     <p className="flex gap-2 px-2 p-3 m-2"> 
+     <MdOutlineSportsSoccer className="text-2xl">
+        </MdOutlineSportsSoccer>sports</p> 
+     <p className="flex ">
+        <CiCalendarDate className="m-1 text-3xl"> </CiCalendarDate> 
+        {moment().format("dddd, MMMM D YYYY ")} </p> 
+
+
+     <h1>  <img src={category3} alt="" /> 
+     Bayern Slams Authorities Over Flight Delay to Club World Cup</h1>
+   <p className="flex gap-2 px-2 p-3 m-2">
+     <MdOutlineSportsSoccer className="text-2xl">
+        </MdOutlineSportsSoccer>sports</p> 
+   <p className="flex"> <CiCalendarDate className="m-1 text-3xl"></CiCalendarDate> 
+    {moment().format("dddd, MMMM D YYYY ")} </p> 
+    </div> 
+   </div>
+ </div>
+       
     );
 };
 
--- src/pages/Shared/Rightsidenav/Rightsidenav.jsx
+++ src/pages/Shared/Rightsidenav/Rightsidenav.jsx
@@ -1,24 +1,71 @@
 import { FaGoogle } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
+import swiming1 from "../../../assets/qZone1.png";
+import swiming2 from "../../../assets/qZone2.png";
+import swiming3 from "../../../assets/qZone3.png";
+
 const Rightsidenav = () => {
     return (
-        <div className=" space-y-2 " >
+        <div className=" space-y-3  p-4 mb-6" >
   
         <p className="font-poppins font-semibold p-1 text-2xl text-blue-600">Login_With</p>
             
-      <button>
-          <div className="border p-1  m-1 border-cyan-400 rounded-md  py-2 ">
+
+          <div className="border p-1  m-1 border-cyan-400 rounded-md  ">
+          <button className="py-1 ">
           <p className="text-2x flex  text-cyan-500"> <FaGoogle className="mr-1"></FaGoogle> Loin with Google </p>
-          </div>
-                     
+                        
         </button>
-            <button>
-          <div className="border  m-1 border-cyan-400 p-2 rounded-md py-2">
-          <p className="text-2x flex  "> <FaGithub className="mr-1"></FaGithub> Loin with GitHub </p>
           </div>
-           </button>
+       
+           
+          <div className="border  p m-1 border-cyan-400 p-1 rounded-md ">
+          <button className="py-1 ">
+          <p className="text-2x flex  "> <FaGithub className="mr-1"></FaGithub> Loin with GitHub </p>
+          </button>
+          </div>
+
+          <p className="font-poppins font-semibold p-1 text-2xl text-slate-00">Find_US_On</p>
+          <div className="border p-1  m-1 border-cyan-400 rounded-md  ">
+          <button className="py-1 ">
+          <h2 className="text-2x flex  text-cyan-500"> <FaFacebook className="mr-1"></FaFacebook>Facebook </h2>
+                        
+        </button>
+          </div>
+       
+           
+          <div className="border  p m-1 border-cyan-400 p-1 rounded-md ">
+          <button className="py-1 ">
+          <h2 className="text-2x flex  "> <AiFillTwitterCircle className="mr-1 text-slate-600"></AiFillTwitterCircle>Twitter</h2>
+          </button>
+          </div>
+
+          <div className="border  p m-1 border-cyan-400 p-1 rounded-md ">
+          <button className="py-1 ">
+          <h2 className="text-2x flex  "> <FaInstagram className="mr-1 text-orange-400"></FaInstagram>Instagram</h2>
+          </button>
+      
+          </div>
+          {/* q zone  */}
+       
+          <div className=" space-y-3  p-4 mb-6" >
+  <div className="w-full text-gray-600">
+        <h2 className="font-poppins font-semibold p-1 text-2xl text-pink-400">Q_Zone</h2>
+            
+
+        
+       <img src={swiming1} alt="" />
+       <img src={swiming2} alt="" />
+       <img src={swiming3} alt="" />
+       </div>
+          </div>
         </div>
-    );
+     
+     
+    )
 };
 
 export default Rightsidenav;
