b8c0f7fdf4870fa7bae9bb70e2d120e65a48a928
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-05-21 19:48:34 +0600

    setup

--- src/pages/News/NewsPage.jsx
+++ src/pages/News/NewsPage.jsx
@@ -1,12 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import Header from "../Shared/Header/Header";
 import Rightsidenav from "../Shared/Rightsidenav/Rightsidenav";
 import Navbar from "../Shared/Navbar/Navbar";
+import  img6  from "../../assets/editorsInsight1.png";
 
 
 
 const NewsPage = () => {
+
+
     const {id}=useParams();
+    console.log(id);
     return (
     
        <div>
@@ -17,6 +21,8 @@ const NewsPage = () => {
            
             <div className="col-span-3" >
             <h3>new deyails</h3>
+            <img src={img6} alt=""  />
+     
             <p>{id}</p>
             </div>
             <div>
